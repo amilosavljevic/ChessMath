8d4786da1b5efe83166a5b99657b01bba0bc81d0
Mira Milosavljevic <mira.milosavljevic99@gmail.com>
2023-12-09 23:21:22 +0100

    Added grid buttons

--- Assets/Code/Memory/MemoryController.cs
+++ Assets/Code/Memory/MemoryController.cs
@@ -1,7 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MemoryController: MonoBehaviour
 {
     [SerializeField] private Button tileButtonPrefab;
+    [SerializeField] private Transform tileHolder;
+    private List<Button> buttons = new List<Button>();
+    
+    private void Start()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            var button = Instantiate(tileButtonPrefab, tileHolder);
+            buttons.Add(button);
+        }
+    }
+    
 }
---
+++ Assets/Code/Memory/MemoryTileController.cs
@@ -0,0 +1,10 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryTileController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text buttonLabel;
+    [SerializeField] private Button button;
+    
+}

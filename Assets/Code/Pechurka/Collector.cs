d918d3e8d8f7ceca7b2d8bc84895a9c6c198287b
emilijakn <knezevic.emilija@gmail.com>
2024-04-10 21:31:35 +0200

    Pechurka Collector + moving

---
+++ Assets/Code/Pechurka/Collector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Code.Pechurka
+{
+    public class Collector : MonoBehaviour
+    {
+        private Rigidbody2D body2D;
+        [SerializeField] private GameObject bottomRight;
+        [SerializeField] private GameObject bottomLeft;
+        private Vector3 left;
+        private Vector3 right;
+        
+        private void Start()
+        {
+            body2D = GetComponent<Rigidbody2D>();
+            left = bottomLeft.transform.position;
+            right = bottomRight.transform.position;
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            var pechurka = collision.gameObject.GetComponent<Pechurka>();
+            
+            if (pechurka != null) 
+            { 
+                Destroy(pechurka.gameObject);
+            } 
+        }
+
+        private void FixedUpdate()
+        {
+            var xOffset = Input.mousePosition.x / Screen.width;
+            var newPosition = Vector3.Lerp(left, right, xOffset);
+            body2D.MovePosition(newPosition);
+        }
+    }
+}
---
+++ Assets/Code/Pechurka/Pechurka.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Code.Pechurka
+{
+    public class Pechurka : MonoBehaviour
+    {
+        
+    }
+}
